e6b0168dd1dcc86cd0e709894b022161a319ae4f
dev-cocalee <cocolitalee@gmail.com>
2023-08-15 18:03:52 +0100

    first task done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints some string
+ * Return: 0
+ * 
+*/
+int main(void)
+{
+char *val = "_putchar\n";
+int i = 0;
+while (val[i])
+_putchar(val[i]), i++;
+return (0);
+}
