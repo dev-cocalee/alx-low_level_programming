51e8671cdae42f74a16e7381bb8ab1867c8e4b5b
dev-cocalee <cocolitalee@gmail.com>
2023-08-18 00:17:47 +0100

    pity me tonight, Mr Checker

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,8 +4,12 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+
 int main(void)
 {
-positive_or_negative(0);
+int i;
+
+i = 0;
+positive_or_negative(i);
 return (0);
 }
