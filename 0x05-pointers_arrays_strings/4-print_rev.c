d0c6f709429d8c3fbc01024395e4b7d9930245ef
Zeecaptcha <ayomiposijimoh@gmail.com>
2023-08-22 02:40:12 +0100

    apparently, ms betty isn't enough

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,15 +7,6 @@
 void print_rev(char *s)
 {
 int i = 0;
-char *err_msg = "(null)";
-
-if (s == NULL)
-{
-while (err_msg[i])
-write(2, &err_msg[i], 1), i++;
-_putchar(10);
-return;
-}
 
 while (s[i])
 i++;
