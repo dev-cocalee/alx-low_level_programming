89375565b9d43ed1e9fc48e3c7f97e8fc69bb1f3
Zeecaptcha <ayomiposijimoh@gmail.com>
2023-09-06 02:50:49 +0100

    check these first, checker

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,61 +10,44 @@
 */
 char **strtow(char *str)
 {
-int i = 0, w = 0, j = 0, c = 0, k = 0, in = 0;
+int i = 0,  j = 0, k = 0, l = 0, m = 0, in = 0;
 char **new_str;
 if (str == NULL)
 return (NULL);
-/*w checks for number of words while i checks for num of valid bytes within str*/
+
 while (str[i] != '\0')
 {
 if (str[i] != 32)
 i++;
 if (str[i] != 32 && str[i - 1] == 32)
-w++;
+l++;
 }
 
-new_str = (char **)malloc(i * sizeof(char) + w);
+new_str = (char **)malloc(i *sizeof(char) + l);
 
 if (new_str == NULL)
 return (NULL);
 else
 {
-for (i = 0; i < w; i++)
+for (i = 0; i < l; i++)
 {
-c = 0;
-for ( ; c == 0; j++)
+m = 0;
+for ( ; m == 0; j++)
 {
 if (str[j] != 32 && str[j - 1] == 32)
 {
 k = j;
 for ( ; str[j + 1] != 32; j++)
-c++;
+m++;
 }
 }
-new_str[i] = (char *)malloc((c + 1) * sizeof(char) + 1);
+new_str[i] = (char *)malloc((m + 1) * sizeof(char) + 1);
 if (new_str[i] == NULL)
-return(NULL);
+return (NULL);
 else
 for ( ; str[k] != 32 && str[k] != '\0'; k++, in++)
 new_str[i][in] = str[k];
 }
 return (new_str);
 }
-}
-
-
-/**
- * print_tab - Prints an array of string
- * @tab: The array to print
- *
- * Return: nothing
- */
-void print_tab(char **tab)
-{
-    int i;
-
-    for (i = 0; tab[i] != NULL; ++i)
-    {
-        printf("%s\n", tab[i]);
-    }
 }
