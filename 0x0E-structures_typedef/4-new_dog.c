83b48084825e1dfec0c66aa06559b2a1ac210fa0
Zeecaptcha <ayomiposijimoh@gmail.com>
2023-09-12 00:01:09 +0100

    tired

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - returns the length of a string
- * @arr: argument
+ * @arr: array to be counted
  * Return: length of arr
 */
 int _strlen(char *arr)
@@ -15,8 +15,8 @@ return (i);
 
 /**
  * _strcpy - copies src to dest
- * @dest: buffer to be filled up
- * @src: string to fill dest up with
+ * @dest: destination
+ * @src: source
  * Return: dest
 */
 char *_strcpy(char *dest, char *src)
@@ -47,16 +47,6 @@ newdoggy = (dog_t *)malloc(sizeof(dog_t));
 if (newdoggy == NULL)
 return (NULL);
 
-/*first member*/
-len = _strlen(name);
-newdoggy->name = (char *)malloc((len *sizeof(char)));
-if (newdoggy->name == NULL)
-{
-free(newdoggy);
-return (NULL);
-}
-
-/*third member*/
 len = _strlen(owner);
 newdoggy->owner = (char *)malloc((len *sizeof(char)));
 if (newdoggy->owner == NULL)
@@ -67,6 +57,16 @@ return (NULL);
 }
 
 newdoggy->owner = _strcpy(newdoggy->owner, owner);
+
+len = _strlen(name);
+newdoggy->name = (char *)malloc((len *sizeof(char)));
+if (newdoggy->name == NULL)
+{
+free(newdoggy->owner);
+free(newdoggy);
+return (NULL);
+}
+
 newdoggy->name = _strcpy(newdoggy->name, name);
 newdoggy->age = age;
 return (newdoggy);
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,6 +25,8 @@ void print_dog(struct dog *d);
 */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *arr);
 void free_dog(dog_t *d);
 
 #endif /*DOG_H*/
