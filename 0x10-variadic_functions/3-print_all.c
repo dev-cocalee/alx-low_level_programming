a79af80e1c9b3328bfc4362a1c20d76fcedbeb31
Zeecaptcha <ayomiposijimoh@gmail.com>
2023-09-14 21:06:30 +0100

    breathe in, breathe out

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,40 +4,39 @@
  * print_all - prints anything
  * @format: variadic argument and list of arguments to the function
  */
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
 void print_all(const char *const format, ...)
 {
 size_t i = 0;
 char *d;
 va_list ments;
-va_start(ments, format);
 
-if (format == NULL)
-return;
+va_start(ments, format);
 
 while (format[i] && format)
 {
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(ments, int));
-break;
-case 'i':
-printf("%d", va_arg(ments, int));
-break;
-case 'f':
-printf("%f", va_arg(ments, double));
-break;
-case 's':
-d = va_arg(ments, char *);
-if (d == NULL)
-d = "(nil)";
-printf("%s", d);
-}
-if ((format[i] == 's' || format[i] == 'f' ||
-format[i] == 'c' || format[i] == 'i') && format[i + 1] != '\0')
+(format[i] == 'c') ? printf("%c", va_arg(ments, int))
+: (format[i] == 'i') ? printf("%d", va_arg(ments, int))
+: (format[i] == 'f') ? printf("%f", va_arg(ments, double))
+: (format[i] == 's') ? (d = va_arg(ments, char *),
+(d == NULL) ? printf("(nil)") : printf("%s", d))
+: 0;
+
+if ((format[i] == 's' || format[i] == 'f' || format[i] == 'c' ||
+format[i] == 'i') && format[i + 1] != '\0')
 printf(", ");
 i++;
 }
+
 printf("\n");
 va_end(ments);
+}
+
+int main(void)
+{
+print_all(NULL);
+return (0);
 }
